d23e811f5bf9d06d13efd719603cdb0756b44af6
PRESCO\bunny.huang <wleinh@gmail.com>
2018-07-26 17:52:20 +0800

    pick range date

--- app/script/datePicker/DatePicker.component.jsx
+++ app/script/datePicker/DatePicker.component.jsx
@@ -11,7 +11,8 @@ class InputDate extends React.Component {
     render() {
         return (
             <div>
-                <input type="text" readOnly value={this.props.selectedDate} />
+                <input type="text" value={this.props.startDate} placeholder="StartDate" />
+                <input type="text" value={this.props.endDate} placeholder="EndDate" />
             </div>
         );
     }
@@ -20,85 +21,163 @@ class InputDate extends React.Component {
 class SelectDate extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            tempEndDate: ''
+        }
     }
 
-    handleChangeMonth = number => {
-        let newDate = moment(this.props.selectedDate).add(number, 'month');
+    handleMouseOver = tempEndDate => {
+        if (this.props.startDate === "" && this.props.endDate === "") {
+            this.setState({ tempEndDate: tempEndDate });
+        }
 
-        this.props.handleChangeDate(newDate);
+        if (this.props.startDate !== "" && this.props.endDate === "") {
+            this.setState({ tempEndDate: tempEndDate });
+        }
+    }
+
+    handleMouseLeave = () => {
+        this.setState({ tempEndDate: '' });
+    }
+
+    handleCalendarActive = active => {
+        this.props.handleCalendarActive(active);
+    }
+
+    handleChangeFirstMonth = number => {
+        let newFirstMonth = moment(this.props.firstMonth).add(number, 'month');
+
+        this.props.handleChangeFirstMonth(newFirstMonth);
     };
 
-    handleChangeDay = newDay => {
-        if (newDay > 0 && newDay <= moment(this.props.selectedDate, "YYYY-MM").daysInMonth()){
-            let newDate = new Date(moment(this.props.selectedDate).year(), moment(this.props.selectedDate).month(), newDay);
+    handleChange = (newDate) => {
+        this.props.handleChange(moment(newDate).format('YYYY-MM-DD'));
+    };
 
-            this.props.handleChangeDate(newDate);
+    handleIsInSelectedRange = date => {
+
+        const startDate = this.props.startDate;
+        const endDate = this.props.endDate !== '' ? this.props.endDate : this.state.tempEndDate;
+
+        if (startDate !== "" && endDate !== "") {
+            return date >= this.props.startDate && date <= endDate;
+        }
+        else if (startDate !== "") {
+            return date === this.props.startDate;
+        }
+        else {
+            return false;
         }
     };
 
+    handleIsInToday = date => {
+        if (date === moment().format('YYYY-MM-DD'))
+            return true;
+        else
+            return false;
+    };
+
+    componentDidMount(){
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+    
+    handleClickOutside = event => {
+        if (this.el && !this.el.contains(event.target)) {
+            this.handleCalendarActive(false);
+
+            document.removeEventListener('mousedown', this.handleClickOutside);
+        }
+    };
+    
     render() {
 
-        const year = moment(this.props.selectedDate).year();
-        const month = moment(this.props.selectedDate).month() + 1;
-        const day = moment(this.props.selectedDate).date();
-        const daysInMonth = moment(this.props.selectedDate, "YYYY-MM").daysInMonth();
-        const firstDayInWeek = parseInt(moment(this.props.selectedDate).startOf('month').format('e'));
-        const weekCount = (daysInMonth + firstDayInWeek - 1) / 7;
-
-        console.log(daysInMonth);
         return (
-            <div>
-                <div>
-                    <i className="fa fa-arrow-left" onClick={() => this.handleChangeMonth(-1)}></i>
-                    <span>
-                        {year} 年 {month} 月
-                    </span>
-                    <i className="fa fa-arrow-right" onClick={() => this.handleChangeMonth(1)}></i>
-                </div>
-                <div>
-                    <div className="week">
-                        <div className="unselected">日</div>
-                        <div className="unselected">一</div>
-                        <div className="unselected">二</div>
-                        <div className="unselected">三</div>
-                        <div className="unselected">四</div>
-                        <div className="unselected">五</div>
-                        <div className="unselected">六</div>
-                    </div>
-                    {
-                        _.map(_.range(0, weekCount), (w) => {
-                            
-                            return (
-                                <div className="week">
-                                    {
-                                        _.map(_.range(1 - firstDayInWeek + w * 7, 1 - firstDayInWeek + 7 + w * 7), (d) => {
-                                            
-                                            return (
-                                                <div key={`item${d}`}
-                                                    className={classNames({ "selected": day === d }, { "unselected": day !== d })}
-                                                    onClick={() => this.handleChangeDay(d)}
-                                                >
-                                                    {d > 0 && d <= daysInMonth ? d : ""}
-                                                </div>
-                                            );
-                                        })
-                                    }
+            <div className="calendar" ref={el => (this.el = el)}>
+                {
+                    _.map(_.range(0, 2), (m) => {
+                        const viewDate = moment(this.props.firstMonth).add(m, 'month');
+                        const daysInMonth = moment(viewDate).daysInMonth();
+                        const firstDayInWeek = parseInt(moment(viewDate).startOf('month').format('e'));
+                        const weekCount = (daysInMonth + firstDayInWeek) / 7;
+
+                        let isFirstMonth = true;
+                        if (m !== 0) isFirstMonth = false;
+
+                        return (
+                            <div className="month" key={`month${m}`}>
+                                <i
+                                    className={classNames("fa fa-arrow-left", { "hide": !isFirstMonth })}
+                                    onClick={() => this.handleChangeFirstMonth(-1)}
+                                />
+                                <span className="head">{moment(viewDate).year()} 年 {moment(viewDate).month() + 1} 月</span>
+                                <i
+                                    className={classNames("fa fa-arrow-right", { "hide": isFirstMonth })}
+                                    onClick={() => this.handleChangeFirstMonth(1)}
+                                />
+
+                                <div className="week-head">
+                                    <div className="day">日</div>
+                                    <div className="day">一</div>
+                                    <div className="day">二</div>
+                                    <div className="day">三</div>
+                                    <div className="day">四</div>
+                                    <div className="day">五</div>
+                                    <div className="day">六</div>
                                 </div>
-                            );
-                        })
-                    }
-                </div>
-            </div >
+                                {
+                                    _.map(_.range(0, weekCount), (w) => {
+
+                                        return (
+                                            <div className="week" key={`week${w}`}>
+                                                {
+                                                    _.map(_.range(1 - firstDayInWeek + w * 7, 1 - firstDayInWeek + 7 + w * 7), (d) => {
+                                                        const date = moment(new Date(moment(viewDate).year(), moment(viewDate).month(), d)).format('YYYY-MM-DD');
+
+                                                        if (d > 0 && d <= daysInMonth) {
+                                                            return (
+                                                                <div key={`day${d}`}
+                                                                    className={classNames('day',
+                                                                        { "today": this.handleIsInToday(date) },
+                                                                        { "selected": this.handleIsInSelectedRange(date) })}
+                                                                    onClick={() => this.handleChange(date)}
+                                                                    onMouseOver={() => this.handleMouseOver(date)}
+                                                                    onMouseLeave={() => this.handleMouseLeave()}
+                                                                >
+                                                                    {moment(date).date()}
+                                                                </div>
+                                                            );
+                                                        }
+                                                        else {
+                                                            return (
+                                                                <div key={`day${d}`} className="other-month"></div>
+                                                            );
+                                                        }
+                                                    })
+                                                }
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </div >
+                        )
+                    })
+                }
+            </div>
         );
     }
 }
 
 InputDate.defaultProps = {
-    selectedDate: moment().format('YYYY-MM-DD')
+    startDate: '',
+    endDate: ''
 }
 
 SelectDate.defaultProps = {
-    selectedDate: moment().format('YYYY-MM-DD')
+    calendarActive: false,
+    firstMonth: moment().format('YYYY-MM'),
+    startDate: '',
+    endDate: ''
 }
 
 export { InputDate, SelectDate };
--- app/script/datePicker/DatePicker.jsx
+++ app/script/datePicker/DatePicker.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import classNames from 'classnames';
 import { InputDate, SelectDate } from './DatePicker.component';
 
 
@@ -9,24 +10,54 @@ class DatePicker extends React.Component {
         super(props);
 
         this.state = {
-            selectedDate: moment().format('YYYY-MM-DD')
+            calendarActive: false,
+            firstMonth: moment().format('YYYY-MM-DD'),
+            startDate: '',
+            endDate: ''
         }
     }
 
-    handleChangeDate = newDate => {
-        this.setState({ selectedDate: moment(newDate).format('YYYY-MM-DD') });
+    handleChangeFirstMonth = newFirstMonth => {
+        this.setState({ firstMonth: moment(newFirstMonth).format('YYYY-MM') });
+    };
+
+    handleCalendarActive = active => {
+        this.setState({ calendarActive: active });
+    }
+
+    handleChange = newDate => {
+        if (this.state.startDate === '' || (this.state.startDate !== '' && this.state.endDate !== '')) {
+            this.setState({ startDate: moment(newDate).format('YYYY-MM-DD') });
+            this.setState({ endDate: '' });
+        }
+        else {
+            if (newDate <= this.state.startDate) {
+                this.setState({ startDate: moment(newDate).format('YYYY-MM-DD') });
+            }
+            else {
+                this.setState({ calendarActive: false });
+                this.setState({ endDate: moment(newDate).format('YYYY-MM-DD') });
+            }
+        }
     };
 
     render() {
         return (
-            <div>
+            <div onFocus={() => this.handleCalendarActive(true)} >
                 <InputDate
-                    selectedDate={this.state.selectedDate}
+                    startDate={this.state.startDate}
+                    endDate={this.state.endDate}
                 />
-                <SelectDate
-                    selectedDate={this.state.selectedDate}
-                    handleChangeDate={newDate => this.handleChangeDate(newDate)}
-                />
+                {this.state.calendarActive &&
+                    <SelectDate
+                        firstMonth={this.state.firstMonth}
+                        startDate={this.state.startDate}
+                        endDate={this.state.endDate}
+                        handleCalendarActive={active => this.handleCalendarActive(active)}
+                        handleChangeFirstMonth={newDate => this.handleChangeFirstMonth(newDate)}
+                        handleChange={newDate => this.handleChange(newDate)}
+                    />
+                }
             </div>
         );
     }
